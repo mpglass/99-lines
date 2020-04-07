e6280d6f729bd139c66ebcb776a47897d7e9f39e
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-07 17:26:04 -0500

    added array and notes for function setup

---
+++ app.js
@@ -0,0 +1,14 @@
+let friends = ['Jessica', 'Luke', 'Adam', 'Lady Fluffington', 'Nibby'];
+//add function to make the song sing 100 console logs
+    //add function within 1st to make one friend be selected at the time 
+        //have verses of song list selected friend's name and count backwards from 99 to 0
+            //have verse with '1 line of code' change gramar to singular
+                
+function singSong () {
+    var text = '';
+    var i; 
+    for (i = 0; i < 99; i++) {
+        text= i + ' lines of code in the file, ' + i + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + i-1 + ' lines of code in the file';
+    }
+}
+console.log(singSong)
